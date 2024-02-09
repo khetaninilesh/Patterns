e2d8f48d4e8f2a43e63a5823f84e393900783f89
nileshdeep2312 <nilesh.khetani@deepcoder.in>
2024-02-09 17:39:32 +0530

    Hollow Butterfly

--- src/Learning/Advanced_Pattern.java
+++ src/Learning/Advanced_Pattern.java
@@ -39,6 +39,43 @@ public class Advanced_Pattern {
             System.out.println();
         }*/
 //        Print Hollow butterfly Pattern
+        for (int i = 1; i <= raw; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j==1||j==i)
+                System.out.print("*");
+                else System.out.print(" ");
+            }
+//            Space
+            int space = 2 * (raw - i);
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                if (j==1||j==i)
+                System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+//        Lower Half
+        for (int i = raw; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                if (j==1||j==i)
+                System.out.print("*");
+                else System.out.print(" ");
+            }
+//            Space
+            int space = 2 * (raw - i);
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                if (j==1||j==i)
+                System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
 //        Diamond Pattern
        /* for (int i = 1; i <= raw; i++) {
             for (int j = 1; j <= raw - i; j++) {
