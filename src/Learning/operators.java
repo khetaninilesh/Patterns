b62d3f3df7e8ccbc8721936486411e3e1a3a0701
nileshdeep2312 <nilesh.khetani@deepcoder.in>
2024-02-12 18:19:06 +0530

    operators

---
+++ src/Learning/operators.java
@@ -0,0 +1,19 @@
+package Learning;
+
+import java.util.Scanner;
+
+public class operators {
+    public static void main(String[] args) {
+        Scanner abc = new Scanner(System.in);
+        int a, b;
+        System.out.println("Please enter value of 'a'");
+        a = abc.nextInt();
+        System.out.println("Please enter value of 'b'");
+        b = abc.nextInt();
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(~a);
+
+    }
+}
