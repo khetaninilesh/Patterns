da808cddcc5197deff752f85b03f1a52b8413170
nileshdeep2312 <nilesh.khetani@deepcoder.in>
2024-02-13 10:03:54 +0530

    calculation

--- src/Learning/cal.java
+++ src/Learning/cal.java
@@ -5,9 +5,17 @@ import java.util.Scanner;
 public class cal {
     public static void main(String[] args) {
         Scanner ax = new Scanner(System.in);
-        int a = ax.nextInt();
-        int b = ax.nextInt();
-        int c = ax.nextInt();
-        System.out.println("Average value = "+ (a+b+c)/3);
+        int a, b, c;
+        System.out.print("a = ");
+        a = ax.nextInt();
+        System.out.print("b = ");
+        b = ax.nextInt();
+        System.out.print("c =");
+        c = ax.nextInt();
+
+//        Enter 3 numbers from the user & make a function to print their average.
+        System.out.println("sum value = " + (a + b + c));
+        System.out.println();
     }
 }
+
